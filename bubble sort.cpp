ae8526669ac9a0fbe7b85fba4cc177202e2d79ab
nikshan166 <nikshanch10@gmail.com>
2025-08-26 09:02:45 +0530

    Create bubble sort.cpp

---
+++ bubble sort.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={3,10,5,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+i]);
+                
+            }
+        }
+    }
+for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+    
+}
+}
